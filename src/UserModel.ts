fa51c2817e65b9ffed3b79c4ffacd68a8dd34952
bitfishxyz <bitfishxyz@gmail.com>
2019-06-07 11:14:29 +0800

    添加IUser接口作为userModel的泛型参数 使得后续可以做类型检查

--- src/UserModel.ts
+++ src/UserModel.ts
@@ -1,7 +1,34 @@
 import mongoose from "./mongoose";
-import UserSchema from "./UserSchema";
 
-// 创建一个collection
-const User = mongoose.model("User", UserSchema);
+const Schema = mongoose.Schema;
+
+/**
+ * 创建一个文档约束
+ */
+export const UserSchema = new Schema({
+    name: String,
+    age: Number,
+});
+
+/**
+ * 创建一个接口方便类型检查
+ */
+export interface IUser extends mongoose.Document {
+    name: string;
+    age: number;
+}
+
+/**
+ * 创建一个模型对象
+ *   同时传入了IUser这个泛型参数
+ *   以后new User() 创建的document对象就可以做类型检查了
+ */
+const User = mongoose.model<IUser>("User", UserSchema);
 
 export default User;
+
+/**
+ * 这里也可以看到ts的一个缺点
+ *   ts的类型系统是编译阶段的，运行时的js是弱类型的
+ *   导致需要同时声明Schema和interface，增加了重复工作
+ */
